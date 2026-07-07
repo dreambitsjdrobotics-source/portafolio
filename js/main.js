4cd150bb7cb169c5d347cc941bcd632cab4f8f3e
Dream bits <dreambitsjdrobotics@gmail.com>
2026-07-06 23:22:17 -0500

    agrege el envio de correos tercerizado

--- js/main.js
+++ js/main.js
@@ -380,3 +380,44 @@ function observarAnimaciones(contenedor){
 }
 
 observarAnimaciones()
+
+// mandar correo
+
+
+const form = document.getElementById('form');
+const submitBtn = form.querySelector('button[type="submit"]');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    formData.append("access_key", "3beba88b-7f30-4a9d-b43c-0fa1928556ff");
+
+    const originalText = submitBtn.textContent;
+
+    submitBtn.textContent = "Sending...";
+    submitBtn.disabled = true;
+
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert("Success! Your message has been sent.");
+            form.reset();
+            window.location.href = "../index.html#contacto";
+        } else {
+            alert("Error: " + data.message);
+        }
+
+    } catch (error) {
+        alert("Something went wrong. Please try again.");
+    } finally {
+        submitBtn.textContent = originalText;
+        submitBtn.disabled = false;
+    }
+});
